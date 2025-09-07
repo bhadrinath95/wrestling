655d9e7dd8234a68868bab96bb7780547c6d1be5
bhadrinath95 <bhadrinath95@gmail.com>
2025-09-08 00:02:57 +0530

    third commit

--- wrestling/settings.py
+++ wrestling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ov%a#pi9a5(zp+=*_-p51u@d%zzf2cuhbvin*wkpq%_^jkwq^0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'bwe.pythonanywhere.com']
 
 
 # Application definition

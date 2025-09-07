8756c12064a5a88f4cb8cb695d23fa37e42ed492
bhadrinath95 <bhadrinath95@gmail.com>
2025-09-08 00:01:32 +0530

    third commit

--- match/models.py
+++ match/models.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class SingleMatch(models.Model):
     name = models.CharField(max_length=200)
+    date = models.DateField()
     player_1 = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True, related_name="single_match_player_1")
     player_2 = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True, related_name="single_match_player_2")
     winner = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True, related_name="single_match_winner")

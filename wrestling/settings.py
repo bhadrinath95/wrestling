e9fd301e88854656998f222537eba0a77fbf4fed
bhadrinath95 <bhadrinath95@gmail.com>
2025-09-13 00:29:14 +0530

    third commit

--- wrestling/settings.py
+++ wrestling/settings.py
@@ -113,6 +113,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+SESSION_COOKIE_AGE = 60 * 60 
+
+SESSION_SAVE_EVERY_REQUEST = True
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
